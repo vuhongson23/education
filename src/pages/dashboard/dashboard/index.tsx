af92f0a6426fba92fd5a69fbc06d541bb3878821
vuhongson23 <sonvuvt123@gmail.com>
2025-08-05 09:41:47 +0700

    continue project

--- src/pages/dashboard/dashboard/index.tsx
+++ src/pages/dashboard/dashboard/index.tsx
@@ -1,5 +1,26 @@
+import classNames from "classnames/bind";
+
+import styles from "./DashboardPage.module.scss";
+import { UserSolidIcon } from "~/assets/icons";
+
+const cx = classNames.bind(styles);
+
 const DashboardPage = () => {
-  return <div>Dashboard page</div>;
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("summary")}>
+        <div className={cx("card-info")}>
+          <div className={cx("card-info--header")}>
+            <span>
+              <UserSolidIcon />
+            </span>
+            Users
+          </div>
+          <div className={cx("card-info--count")}>200</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
